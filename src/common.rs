afd890b5187e8b9da0b9fc83098bc280b0702701
Caleb Bassi <calebjbassi@gmail.com>
2018-11-20 17:56:07 -0800

    Update query_rofi

--- src/common.rs
+++ src/common.rs
@@ -5,33 +5,32 @@ use std::process::{Command, Stdio};
 pub fn query_rofi(prompt: &str, options: Option<Vec<String>>) -> Option<String> {
     let input = match options {
         Some(x) => x.join("\n"),
-        None => "".to_owned(),
+        None => String::new(),
     };
-    let input_bytes = input.as_bytes();
     let args = ["-dmenu", "-p", prompt, "-i"];
     info!("Running command: `rofi {}`", args.join(" "));
-    let process = Command::new("rofi")
+    let child = Command::new("rofi")
         .args(&args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
-        .expect("could not execute rofi command");
-    process
+        .expect("failed to start rofi command");
+    child
         .stdin
-        .expect("could not connect to rofi stdin")
-        .write_all(input_bytes)
-        .expect("could not write to rofi stdin");
-    let mut s = String::new();
-    process
+        .expect("failed to connect to rofi stdin")
+        .write_all(input.as_bytes())
+        .expect("faild to write to rofi stdin");
+    let mut output = String::new();
+    child
         .stdout
-        .expect("could not connect to rofi stdou")
-        .read_to_string(&mut s)
-        .expect("could not read from rofi stdin");
-    s = s.trim().to_owned();
-    if s == "" {
-        return None;
+        .expect("failed to connect to rofi stdout")
+        .read_to_string(&mut output)
+        .expect("failed to read from rofi stdout");
+    output = output.trim().to_owned();
+    match output.as_ref() {
+        "" => None,
+        _ => Some(output),
     }
-    Some(s)
 }
 
 // https://stackoverflow.com/questions/51344951/how-do-you-unwrap-a-result-on-ok-or-return-from-the-function-on-err
